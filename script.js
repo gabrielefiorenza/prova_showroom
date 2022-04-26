c7930d8997b722fdbc1b14c51cf5a46edeb3251d
gabrielefiorenza <72872864+gabrielefiorenza@users.noreply.github.com>
2022-04-26 14:27:30 +0300

    refactory

--- script.js
+++ script.js
@@ -11,38 +11,3 @@ function myFunction2() {
    video2.style="z-index: 0;"
    video2.play();
 }
-
-
-
-
-
-
-
-
-
-
-
-function Eva() {
-   //var image_x = document.getElementById('img');
-   //image_x.parentNode.removeChild(image_x);
-   
-   const video = document.createElement('video');
-
-// 👇️ Local file
-// video.src = 'video.mp4';
-
-// 👇️ Remote file
-video.src =
-  'render_buono2.mp4';
-
-
-video.autoplay = true;
-video.muted = false;
-video.preload = "auto";
-video.id = "myVideo";
-
-const box = document.getElementById('box');
-
-box.appendChild(video);
-
-}
